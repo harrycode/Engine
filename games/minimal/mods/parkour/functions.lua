7548a7550a3fe1d82ee55fb8f8fce7a24ddc0686
harrycode <pacboy2004@gmail.com>
2018-02-09 22:08:50 +1000

    fixed

--- games/minimal/mods/parkour/functions.lua
+++
@@ -1,9 +0,0 @@
-function newNode (modName, nodeName, nodeTexture, isNaturalResource, nodeGroups, nodeDrops)
-  minetest.register_node(modName:nodeID, {
-      description = nodeName,
-      tiles = nodeTexture,
-      is_ground_content = isNaturalResource,
-      groups = nodeGroups,
-      drops = nodeDrops
-  })
-end
